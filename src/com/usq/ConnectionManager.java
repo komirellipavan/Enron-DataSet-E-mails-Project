2aea2dab9be5e11d063cb22610406a66624bf750
Nischal Sharma <nischal123@gmail.com>
2020-03-24 20:06:46 +1000

    script to read all files and write it into database

---
+++ src/com/usq/ConnectionManager.java
@@ -0,0 +1,31 @@
+package com.usq;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+	    private static String url = "jdbc:mysql://localhost:3306/enron_email";    
+	    private static String driverName = "com.mysql.jdbc.Driver";   
+	    private static String username = "root";   
+	    private static String password = "";
+	    private static Connection con;
+	    private static String urlstring;
+
+	    public static Connection getConnection() {
+	        try {
+	            Class.forName(driverName);
+	            try { 
+	            	con=DriverManager.getConnection("jdbc:mysql://localhost:3306/enron_email","root",""); 
+	            } catch (SQLException ex) {
+	                System.out.println("Failed to create the database connection."); 
+	            }
+	        } catch (ClassNotFoundException ex) {
+	            // log an exception. for example:
+	            System.out.println("Driver not found."); 
+	        }
+	        return con;
+	    }
+	}
+
+

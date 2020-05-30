a071de1584ca36b35476c1206dfa16d6cbedaad9
Pavan <komirellipavan@happytoo.com.au>
2020-05-30 18:49:01 +1000

    user network

--- src/com/usq/DataCleaning.java
+++ src/com/usq/DataCleaning.java
@@ -25,14 +25,18 @@ public class DataCleaning {
 	ResultSet rs;
 	String dd;
 	static int from_index;
+	static int subject_index;
+	static int To_index;
 	
 	
 	public static Message extractKey(Message msg,String messageMetaData) {
 		from_index=0;
+		subject_index =0;
+		To_index=0;
 		
 
 		 try (Scanner scanner = new Scanner(messageMetaData)) {
-			 String mess = "",u_to="",u_from="",subject="",m_date="";
+			 String mess = "",u_to="",u_from="",subject="",m_date="",file="";
 		        
 			 
 			 while (scanner.hasNextLine()) {
@@ -54,14 +58,19 @@ public class DataCleaning {
 		              break;
 		            case ("To"):
 		            	u_to=arrOfStr[1];
+		            	To_index= arrOfStr[0].length()+2;
 		              break;
 		            case ("Subject"):
 		            	subject=arrOfStr[1];
+		            	subject_index = arrOfStr[1].length();
 		              break;
 		            case ("X-FileName"):
 //		            	this is to find the start index to grab the message body
 		            	from_index=arrOfStr[0].length()+arrOfStr[1].length()+2;
-//		            	subject=arrOfStr[1];
+		            	
+		              break;
+		            case ("X-Folder"):
+		            	file=arrOfStr[1];
 		              break;
 		            default:
 //		              System.out.println("no cases");
@@ -81,11 +90,48 @@ public class DataCleaning {
 			 msg.subject=subject;
 			 msg.date= msg.setDate(m_date);;
 			 
-	//		 System.out.println(mess);
+			 
+			// Get the regex to be checked 
+		        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/="
+		        		+ "?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23"
+		        		+ "-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@"
+		        		+ "(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*"
+		        		+ "[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9"
+		        		+ "]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-"
+		        		+ "9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]"
+		        		+ "|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"; 
+		  
+		     // Create a pattern from regex 
+		      Pattern pattern 
+		            = Pattern.compile(regex); 
+		      Matcher matcher 
+	            = pattern 
+	                  .matcher(u_to); 
+	  
+	        // Get the subsequence 
+	        // using find() method 
+	        if(matcher.find()) {
+	        	 
+	        	 int len2 =messageMetaData.indexOf("To: ");
+				 int len1 =messageMetaData.indexOf("Subject");
+				 String res = messageMetaData.substring(len2+To_index,len1);
+				// System.out.println(res.trim());
+				 msg.to_user=res.trim();
+	        }else {
+	        	 msg.to_user="undisclosed";
+	        }
+		      
+			 //System.out.println(mess);
 //    		 System.out.println(m_date);
-//			 System.out.println(u_from);
-//			 System.out.println(u_to);
+			 System.out.println("from:"+ u_from);
+			 System.out.println("To:"+ msg.to_user);
+			 //System.out.println(u_to);
+			// System.out.println(file);
 //			 System.out.println(subject);
+			 
+			 
+		
+			 
 		    }
 		 
 		 
@@ -179,6 +225,29 @@ public class DataCleaning {
 		
 	}
 	
+	public static void insertFromTo(PreparedStatement pstmt, Message msg,int counter) {
+		
+		try {
+			
+			pstmt.setString(1, msg.from_user);
+			pstmt.setString(2, msg.to_user);
+			pstmt.addBatch();
+			
+			if(counter%500==0) {
+
+				pstmt.executeBatch(); 
+				connection.commit(); 
+				System.out.println("records inserted = "+counter); 
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 
 	
 	
@@ -214,6 +283,9 @@ public class DataCleaning {
 		String sqlQuery3 = "insert into message_raw (id,raw_msg) values (?,?)";
 		PreparedStatement pstmt3 = (PreparedStatement) connection.prepareStatement(sqlQuery3);
 		
+		String sqlQuery_insertFromTo = "INSERT INTO `sender_receiver`(`from`, `to`) VALUES (?,?)";
+		PreparedStatement pstmt_insertFromTo = (PreparedStatement) connection.prepareStatement(sqlQuery_insertFromTo);
+		
 		int counter=0,totalmessage=0;;
 		
 		
@@ -226,56 +298,42 @@ public class DataCleaning {
 			
 			if(!msg.body.isEmpty()) {	
 				counter++;
+				insertFromTo(pstmt_insertFromTo,msg,counter);
 //				System.out.println("records inserted "+counter);
 				// add raw msg
-				pstmt1.setInt(1,counter);
-				pstmt1.setString(2, msg.messageid);
-				pstmt1.setString(3, msg.body);
-				pstmt1.addBatch();
 				
-//				System.out.println("records inserted "+counter);
-				//add date
-				pstmt2.setInt(1, counter);
-				try {
-					if(msg.date!=null) {
-						Date msgdate=new java.sql.Date(msg.date.getTime());
-						pstmt2.setDate(2, msgdate);
-					}else {
-						pstmt2.setDate(2, null);
-					}
-					
-					
-				}catch(Exception e) {
-					pstmt2.setDate(2, null);
-				}
+				/*
+				 * pstmt1.setInt(1,counter); pstmt1.setString(2, msg.messageid);
+				 * pstmt1.setString(3, msg.body); pstmt1.addBatch();
+				 * 
+				 * // System.out.println("records inserted "+counter); //add date
+				 * pstmt2.setInt(1, counter); try { if(msg.date!=null) { Date msgdate=new
+				 * java.sql.Date(msg.date.getTime()); pstmt2.setDate(2, msgdate); }else {
+				 * pstmt2.setDate(2, null); }
+				 * 
+				 * 
+				 * }catch(Exception e) { pstmt2.setDate(2, null); }
+				 * 
+				 * pstmt2.addBatch();
+				 * 
+				 * // System.out.println("records inserted "+counter); //add processed message
+				 * pstmt3.setInt(1, counter); pstmt3.setString(2, msg.body); pstmt3.addBatch();
+				 * 
+				 * // System.out.println(msg.from_user); // System.out.println(msg.to_user);
+				 * 
+				 * // System.out.println("=====================\n\n\n");
+				 */				  
+				  
+				  
+				  if(counter%500==0) {
 
-				pstmt2.addBatch();
-				
-//				System.out.println("records inserted "+counter);
-				//add processed message
-				pstmt3.setInt(1, counter);
-				pstmt3.setString(2, msg.body);
-				pstmt3.addBatch();
-				
-//				System.out.println(msg.from_user);
-//				System.out.println(msg.to_user);
-				
-//				System.out.println("=====================\n\n\n");
-
-				
-				
-				if(counter%500==0) {	
-					
-					
-						
-						pstmt1.executeBatch();
-						pstmt2.executeBatch();
-						pstmt3.executeBatch();
-						
-						connection.commit();
-//						msgList.clear();
-						System.out.println("records inserted = "+counter);
-					}
+				  pstmt1.executeBatch(); 
+				  pstmt2.executeBatch();
+				  pstmt3.executeBatch();
+				  
+				  connection.commit(); // msgList.clear();
+				  System.out.println("records inserted = "+counter); }
+				 
 
 				}
 			
@@ -290,21 +348,24 @@ public class DataCleaning {
 		// msgList contains list of message objects
 		//batch insert from here
 		
-		for(Message m : msgList) {
-			//if(m.body!="")
-			//System.out.println(m.toString());
-			//System.out.println(m.toString());
-		}
+
 		
-		pstmt1.executeBatch();
-		pstmt2.executeBatch();
-		pstmt3.executeBatch();
 		
+		/*
+		 * pstmt1.executeBatch(); pstmt2.executeBatch(); pstmt3.executeBatch();
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		pstmt_insertFromTo.executeBatch();
 		connection.commit();
-		
 		System.out.println("total empty message = "+ (totalmessage-counter));
-		
-		
 		connection.close();
 		
 		System.out.println("***********finish************");
